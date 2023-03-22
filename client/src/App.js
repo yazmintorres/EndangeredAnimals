018c512a50c8fe8c05422ce0e75b8db7b72de4af
Yazmin <yazminmoniquetorres@gmail.com>
2023-03-21 22:12:26 -0700

    Add ListSightings component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import "./styles.css";
 import Students from "./components/students";
+import ListSightings from "./components/ListSightings";
 
 function App() {
   return (
     <div className="App">
-      Hello from Techtonica
-      <Students />
+      <header>
+        <h1>Animal Sighting Tracker</h1>
+      </header>
+      <main>
+        <ListSightings />
+      </main>
+      {/* <Students /> */}
     </div>
   );
 }
