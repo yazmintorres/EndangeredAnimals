67223901258dc5d6c3921c02a6b818684f6df013
Yazmin <yazminmoniquetorres@gmail.com>
2023-03-21 22:11:08 -0700

    Create get request for sightings

--- server/server.js
+++ server/server.js
@@ -1,32 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const db = require('./db/db-connection.js');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const db = require("./db/db-connection.js");
 
 const app = express();
 
 const PORT = 8080;
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // creates req.body
 
 // creates an endpoint for the route /api
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from My template ExpressJS' });
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from My template ExpressJS" });
+});
+
+// get sightings
+
+app.get("/api/sightings", async (req, res) => {
+  try {
+    const allSightings = await db.query(
+      "SELECT sightings.sighting_id, sightings.date_time as last_seen, sightings.healthy, sightings.location, individuals.nick_name as name, species.common_name, species.scientific_name FROM sightings LEFT JOIN individuals ON individuals.individual_id = sightings.individual_id LEFT JOIN species ON species.species_id = individuals.species_id"
+    );
+    res.json(allSightings.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 // create the get request
-app.get('/api/students', cors(), async (req, res) => {
-  // const STUDENTS = [
-
-  //     { id: 1, firstName: 'Lisa', lastName: 'Lee' },
-  //     { id: 2, firstName: 'Eileen', lastName: 'Long' },
-  //     { id: 3, firstName: 'Fariba', lastName: 'Dadko' },
-  //     { id: 4, firstName: 'Cristina', lastName: 'Rodriguez' },
-  //     { id: 5, firstName: 'Andrea', lastName: 'Trejo' },
-  // ];
-  // res.json(STUDENTS);
+app.get("/api/students", cors(), async (req, res) => {
   try {
-    const { rows: students } = await db.query('SELECT * FROM students');
+    const { rows: students } = await db.query("SELECT * FROM students");
     res.send(students);
   } catch (e) {
     return res.status(400).json({ e });
@@ -34,28 +38,35 @@ app.get('/api/students', cors(), async (req, res) => {
 });
 
 // create the POST request
-app.post('/api/students', cors(), async (req, res) => {
+app.post("/api/students", cors(), async (req, res) => {
   const newUser = {
     firstname: req.body.firstname,
     lastname: req.body.lastname,
   };
   console.log([newUser.firstname, newUser.lastname]);
   const result = await db.query(
-    'INSERT INTO students(firstname, lastname) VALUES($1, $2) RETURNING *',
-    [newUser.firstname, newUser.lastname],
+    "INSERT INTO students(firstname, lastname) VALUES($1, $2) RETURNING *",
+    [newUser.firstname, newUser.lastname]
   );
   console.log(result.rows[0]);
   res.json(result.rows[0]);
 });
 
-//A put request - Update a student 
-app.put('/api/students/:studentId', cors(), async (req, res) =>{
+//A put request - Update a student
+app.put("/api/students/:studentId", cors(), async (req, res) => {
   console.log(req.params);
   //This will be the id that I want to find in the DB - the student to be updated
-  const studentId = req.params.studentId
-  const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname}
+  const studentId = req.params.studentId;
+  const updatedStudent = {
+    id: req.body.id,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  };
   console.log("In the server from the url - the student id", studentId);
-  console.log("In the server, from the react - the student to be edited", updatedStudent);
+  console.log(
+    "In the server, from the react - the student to be edited",
+    updatedStudent
+  );
   // UPDATE students SET lastname = "something" WHERE id="16";
   const query = `UPDATE students SET lastname=$1, firstname=$2 WHERE id=${studentId} RETURNING *`;
   const values = [updatedStudent.lastname, updatedStudent.firstname];
@@ -63,14 +74,11 @@ app.put('/api/students/:studentId', cors(), async (req, res) =>{
     const updated = await db.query(query, values);
     console.log(updated.rows[0]);
     res.send(updated.rows[0]);
-
-  }catch(e){
+  } catch (e) {
     console.log(e);
-    return res.status(400).json({e})
+    return res.status(400).json({ e });
   }
-})
-
-
+});
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
